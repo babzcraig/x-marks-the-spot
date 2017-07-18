596ea3d36dc1e392e38e02d9da6bae370a88d851
Babs Craig <babscraig@gmail.com>
2017-07-18 12:12:06 +0100

    added google map view with hardcoded coordinates

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+declare var google;
+
 @Component({
-  selector: 'page-home',
+  selector: 'home-page',
   templateUrl: 'home.html'
 })
 export class HomePage {
+
+  @ViewChild('map') mapElement: ElementRef;
+  map: any;
 
   constructor(public navCtrl: NavController) {
 
   }
 
+  ionViewDidLoad(){
+    this.loadMap();
+  }
+
+  loadMap(){
+
+    let latLng = new google.maps.LatLng(-34.9290, 138.6010);
+
+    let mapOptions = {
+      center: latLng,
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    }
+
+    console.log(this.mapElement)
+    this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+
+  }
 }
