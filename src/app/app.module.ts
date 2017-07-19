69a4c8b388f06878cd9c2ff6f52c841b223694b4
Babs Craig <babscraig@gmail.com>
2017-07-19 01:28:57 +0100

    angularfire2, firebase set up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,24 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
+// Import the AF2 Module
+import { AngularFireModule } from 'angularfire2';
+
+// AF2 Settings
+export const firebaseConfig = {
+  apiKey: "AIzaSyDP-LoR9mYoyIQJJClH0hxpGCw6EW2motI",
+  authDomain: "x-marks-the-spot-2a7b4.firebaseapp.com",
+  databaseURL: "https://x-marks-the-spot-2a7b4.firebaseio.com",
+  projectId: "x-marks-the-spot-2a7b4",
+  storageBucket: "x-marks-the-spot-2a7b4.appspot.com",
+  messagingSenderId: "62014686251"
+};
+
+// Pages and Components
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+
+// Providers
 import { Geolocation } from '@ionic-native/geolocation';
 
 @NgModule({
@@ -15,7 +31,8 @@ import { Geolocation } from '@ionic-native/geolocation';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
